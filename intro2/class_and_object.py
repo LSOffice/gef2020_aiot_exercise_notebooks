4c1ca7ff394f1ca878180a8b63859b1a2dcdba99
Toolbot <unconfigured@null.spigotmc.org>
2021-07-31 11:05:41 +0800

    third commit 31/7/2021

--- intro2/class_and_object.py
+++ intro2/class_and_object.py
@@ -23,20 +23,7 @@ class rectangle:
 # square class
 class square(rectangle):
     def __init__(self, length):
-        self.length = length
-
-    def get_perimeter(self):
-        return self.length * 4
-
-    def get_area(self):
-        return self.length * self.length
-
-    def draw(self):
-        rectangle = plt.Rectangle((0, 0), self.length, self.length, facecolor="tab:blue",
-                                  edgecolor="tab:blue", linewidth=3)
-        plt.gca().add_patch(rectangle)
-        plt.axis('equal')
-        plt.show()
+        super().__init__(length, length)
 
 r1 = rectangle(5, 10)
 
