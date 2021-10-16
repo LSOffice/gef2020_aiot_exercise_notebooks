a0295213c195a6685da2164f9c74e399c8167282
Luciano Suen <34027007+LSOffice@users.noreply.github.com>
2021-10-16 11:48:23 +0800

    Add files via upload

--- CityUJupyterNotebook/vitis_ai_pynq_dpu/quantize.py
+++ CityUJupyterNotebook/vitis_ai_pynq_dpu/quantize.py
@@ -20,24 +20,33 @@ try:
 except:
     print('\nError:loading float model failed!')
 
-
-# Write your code here
-#================================================================================
 print("\nLoading the Fashion MNIST dataset...")
 # Loading the Fashion MNIST dataset
-
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
 # Reshaping the data and feature scaling
+x_train = x_train.astype('float32')/255 #feature scaling (Hint:255 is the maximum pixel value)
 
+num_classes = 10 #no. of classes
+
+train_one_hot = tf.keras.utils.to_categorical(y_train,num_classes) #one hot encoding for the training set
+test_one_hot = tf.keras.utils.to_categorical(y_test,num_classes)   #one hot encdoing for the testing set
+
+INPUT_SHAPE = [28,28,1]
+
+x_train = x_train.reshape([-1]+INPUT_SHAPE)
+print(f'Train data shape: {x_train.shape}, Test data shape: {x_test.shape}')
 # Defining the calibration dataset using about 1000 samples from the training set
+calib_dataset = x_train[:1000]
 
 print('\nRunning model quantization...')
 # Running model quantization
 
-
+quantizer = vitis_quantize.VitisQuantizer(float_model)
+quantized_model = quantizer.quantize_model(calib_dataset=calib_dataset) 
 
 # Saving the quantized model
 path = os.path.join(MODEL_DIR, QAUNT_MODEL)
+quantized_model.save(path)
 
 print(f'\nQuantized model has been saved to "{path}"')
 #================================================================================
-
